95dcd4aacee5e6028b8ab11dbab0ae10a1feb8b9
JabezLawrenceG <jabezcs32@gmail.com>
2025-03-13 18:02:21 +0530

    Update main.cpp

--- main/main.cpp
+++ main/main.cpp
@@ -1,29 +1,7 @@
-pipeline {
-    agent any
-    stages {
-        stage('Build') {
-            steps {
-                sh 'g++ main.cpp -o main.out' // Correct file name
-            }
-        }
-        stage('Test') {
-            steps {
-                sh 'chmod +x main.out'
-                sh './main.out'
-            }
-        }
-        stage('Deploy') {
-            steps {
-                echo 'Deployment successful'
-            }
-        }
-    }
-    post {
-        success {
-            echo 'Pipeline Successful!'
-        }
-        failure {
-            echo 'Pipeline Failed'
-        }
-    }
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, Jenkins!" << endl;
+    return 0;
 }
